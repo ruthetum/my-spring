c3af7d71913fa5c41dc2f3a7ccd98310f24ec0b6
Heedong Kim <ruthetum@gmail.com>
2021-10-20 14:38:45 +0900

    [i-jpashop] JPA 동작 확인

---
+++ i-jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("A");
+
+        // when
+        Member savedMember = memberRepository.save(member);
+        Long savedId = savedMember.getId();
+        Member findMember = memberRepository.findById(savedId)
+                .orElseThrow(()->new NullPointerException());
+
+        // then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
