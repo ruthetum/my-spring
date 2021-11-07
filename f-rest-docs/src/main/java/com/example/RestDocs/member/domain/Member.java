ce463a133ff81be837e5a1f96291f209728d889b
Heedong Kim <ruthetum@gmail.com>
2021-11-07 17:07:14 +0900

    [rest-docs] 멤버 엔티티

---
+++ f-rest-docs/src/main/java/com/example/RestDocs/member/domain/Member.java
@@ -0,0 +1,38 @@
+package com.example.RestDocs.member.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Member(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+}
