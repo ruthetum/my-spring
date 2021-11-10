a084787aff511e7292b603db1ca686bfb5d78012
Heedong Kim <ruthetum@gmail.com>
2021-11-11 05:12:40 +0900

    [i-jpashop] 상품 등록

---
+++ i-jpashop/src/main/java/jpabook/jpashop/dto/BookDto.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDto {
+
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private String author;
+    private String isbn;
+}
