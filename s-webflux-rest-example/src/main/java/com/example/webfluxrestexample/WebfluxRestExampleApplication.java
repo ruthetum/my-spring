845122845437c21048fbda7caf07b391b513276a
Heedong Kim <ruthetum@gmail.com>
2022-03-08 19:15:04 +0900

    [webflux-rest-exmaple] 프로젝트 세팅

---
+++ s-webflux-rest-example/src/main/java/com/example/webfluxrestexample/WebfluxRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxrestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxRestExampleApplication.class, args);
+	}
+
+}
