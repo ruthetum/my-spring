5c5da9ebff5a67e5a1ee1f58ca9e92e07b546de2
Heedong Kim <ruthetum@gmail.com>
2022-03-12 20:55:14 +0900

    [spring-batch] chunk 기반 step (itemReader, itemProcessor, itemWriter)

---
+++ f-spring-batch/src/main/java/com/example/springbatch/core/repository/PlainTextRepository.java
@@ -0,0 +1,10 @@
+package com.example.springbatch.core.repository;
+
+import com.example.springbatch.core.domain.PlainText;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlainTextRepository extends JpaRepository<PlainText, Long> {
+    Page<PlainText> findBy(Pageable pageable);
+}
