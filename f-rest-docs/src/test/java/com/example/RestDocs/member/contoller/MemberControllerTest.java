85beb61b7feb2378b48fa9ec7a7b011eb1d0b7c6
Heedong Kim <ruthetum@gmail.com>
2022-04-07 20:39:23 +0900

    [rest-docs] asciidoctor 버전 수정 (gradle 7.x 일 경우 asciidoctor 3.x으로 upgrade)

--- f-rest-docs/src/test/java/com/example/RestDocs/member/contoller/MemberControllerTest.java
+++ f-rest-docs/src/test/java/com/example/RestDocs/member/contoller/MemberControllerTest.java
@@ -1,10 +1,13 @@
 package com.example.RestDocs.member.contoller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -13,6 +16,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ExtendWith({RestDocumentationExtension.class})
 class MemberControllerTest {
 
     @Autowired
