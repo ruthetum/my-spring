a3a64fd783cb17d0299f2bf817734a9d8dc2aab2
wilump(Heedong Kim) <wilump@dunamu.com>
2023-10-18 23:21:11 +0900

    [java-to-kotlin] 리팩토링

---
+++ i-java-to-kotlin/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.java
@@ -0,0 +1,10 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import com.group.libraryapp.calculator.domain.user.loanhistory.UserLoanHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLoanHistoryRepository extends JpaRepository<UserLoanHistory, Long> {
+
+  UserLoanHistory findByBookNameAndIsReturn(String bookName, boolean isReturn);
+
+}
