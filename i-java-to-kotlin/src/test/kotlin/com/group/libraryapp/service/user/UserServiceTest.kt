c6bbdaf43928990b4b1dd02963c72c003b98182a
wilump(Heedong Kim) <wilump@dunamu.com>
2023-10-19 11:34:15 +0900

    [java-to-kotlin] 서비스 리팩토링

--- i-java-to-kotlin/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ i-java-to-kotlin/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,6 +1,6 @@
 package com.group.libraryapp.service.user
 
-import com.group.libraryapp.calculator.domain.user.User
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
@@ -40,7 +40,7 @@ class UserServiceTest @Autowired constructor(
         ))
 
         // when.
-        val results = userService.users
+        val results = userService.getUsers()
 
         // then.
         assertThat(results).hasSize(2)
